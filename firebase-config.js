9ae255c20dadd86d1a6713323d6dd737c98ce0f2
saadabushendihot <saad.abushendi@hotmail.com>
2025-06-24 09:24:56 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+var firebaseConfig = {
+  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw",
+  authDomain: "shefa-502fe.firebaseapp.com",
+  projectId: "shefa-502fe",
+  storageBucket: "shefa-502fe.appspot.com",
+  messagingSenderId: "639314388969",
+  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78"
+};
