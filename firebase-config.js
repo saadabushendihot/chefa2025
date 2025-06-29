e46261c64bc4c3e538092f3ad5cd2148ffc236ce
saadabushendihot <saad.abushendi@hotmail.com>
2025-06-29 12:15:28 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,12 @@
 // firebase-config.js
+// هذا الملف يحتوي على إعدادات مشروع Firebase الخاص بك.
+// يتم استيراده بواسطة صفحات HTML لتهيئة Firebase SDK.
+
 var firebaseConfig = {
-  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw",
-  authDomain: "shefa-502fe.firebaseapp.com",
-  projectId: "shefa-502fe",
-  storageBucket: "shefa-502fe.appspot.com",
-  messagingSenderId: "639314388969",
-  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78"
+  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw", // مفتاح API لمشروعك
+  authDomain: "shefa-502fe.firebaseapp.com", // نطاق المصادقة لمشروعك
+  projectId: "shefa-502fe", // معرّف المشروع الخاص بك
+  storageBucket: "shefa-502fe.firebasestorage.app", // <--- هذا هو التعديل الضروري! اسم سلة التخزين الصحيح
+  messagingSenderId: "639314388969", // معرّف مرسل المراسلة
+  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78" // معرّف التطبيق
 };
