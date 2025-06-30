a9b1eec17507ff6ff7da51effcb351eb1f34ea85
saadabushendihot <saad.abushendi@hotmail.com>
2025-07-01 00:02:18 +0300

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,12 +1,16 @@
-// firebase-config.js
-// هذا الملف يحتوي على إعدادات مشروع Firebase الخاص بك.
-// يتم استيراده بواسطة صفحات HTML لتهيئة Firebase SDK.
+// js/firebase-init.js
 
+// إعدادات مشروع Firebase الخاص بك
 var firebaseConfig = {
-  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw", // مفتاح API لمشروعك
-  authDomain: "shefa-502fe.firebaseapp.com", // نطاق المصادقة لمشروعك
-  projectId: "shefa-502fe", // معرّف المشروع الخاص بك
-  storageBucket: "shefa-502fe.firebasestorage.app", // <--- هذا هو التعديل الضروري! اسم سلة التخزين الصحيح
-  messagingSenderId: "639314388969", // معرّف مرسل المراسلة
-  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78" // معرّف التطبيق
+  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw", // مفتاح API الخاص بك من firebase-config.js
+  authDomain: "shefa-502fe.firebaseapp.com", // نطاق المصادقة الخاص بك من firebase-config.js
+  projectId: "shefa-502fe", // معرّف المشروع الخاص بك من firebase-config.js
+  storageBucket: "shefa-502fe.firebasestorage.app", // اسم سلة التخزين الصحيح من firebase-config.js
+  messagingSenderId: "639314388969", // معرّف مرسل المراسلة الخاص بك من firebase-config.js
+  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78" // معرّف التطبيق الخاص بك من firebase-config.js
 };
+
+// تهيئة Firebase SDK (يجب أن يتم ذلك مرة واحدة فقط)
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
