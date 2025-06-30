a00ab2d8179dae0f80cd05d2b34ded7dd8f59649
saadabushendihot <saad.abushendi@hotmail.com>
2025-06-30 21:15:23 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,18 +2,6 @@
 // هذا الملف يحتوي على إعدادات مشروع Firebase الخاص بك.
 // يتم استيراده بواسطة صفحات HTML لتهيئة Firebase SDK.
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw", // مفتاح API لمشروعك
-  authDomain: "shefa-502fe.firebaseapp.com", // نطاق المصادقة لمشروعك
-  projectId: "shefa-502fe", // معرّف المشروع الخاص بك
-  storageBucket: "shefa-502fe.firebasestorage.app", // <--- هذا هو التعديل الضروري! اسم سلة التخزين الصحيح
-  messagingSenderId: "639314388969", // معرّف مرسل المراسلة
-  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78" // معرّف التطبيق
-};
-// firebase-config.js
-// هذا الملف يحتوي على إعدادات مشروع Firebase الخاص بك.
-// يتم استيراده بواسطة صفحات HTML لتهيئة Firebase SDK.
-
 console.log("firebase-config.js loaded."); // أضف هذا السطر
 // ... (بقية إعدادات firebaseConfig)
 var firebaseConfig = {
@@ -26,3 +14,17 @@ var firebaseConfig = {
 };
 
 console.log("firebaseConfig variable defined:", typeof firebaseConfig !== 'undefined'); // أضف هذا السطر
+
+
+// firebase-config.js
+// هذا الملف يحتوي على إعدادات مشروع Firebase الخاص بك.
+// يتم استيراده بواسطة صفحات HTML لتهيئة Firebase SDK.
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCNx0wz_Xf5kXHl8kxmn7j6eH90DSMsEHw", // مفتاح API لمشروعك
+  authDomain: "shefa-502fe.firebaseapp.com", // نطاق المصادقة لمشروعك
+  projectId: "shefa-502fe", // معرّف المشروع الخاص بك
+  storageBucket: "shefa-502fe.firebasestorage.app", // <--- هذا هو التعديل الضروري! اسم سلة التخزين الصحيح
+  messagingSenderId: "639314388969", // معرّف مرسل المراسلة
+  appId: "1:639314388969:web:ad5245fc2d5d9c750c4d78" // معرّف التطبيق
+};
